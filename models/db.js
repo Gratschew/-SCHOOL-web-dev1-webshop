9f6086f6796eebc8b8bc1ab28c9702b195f59c33
gratsche <max.gratschew@tuni.fi>
2021-11-16 15:46:53 +0200

    9.4 done

--- models/db.js
+++ models/db.js
@@ -10,7 +10,8 @@ const mongoose = require('mongoose');
  */
 const getDbUrl = () => {
   // TODO: 9.4 Implement this
-  throw new Error('Implement this');
+  require('dotenv').config();
+  return process.env.DBURL;
 };
 
 const connectDB = () => {
